948f1d9bf4a131fdb661d5e6dd0c338abaa0f8f4
Malichr <84735312+Malichr@users.noreply.github.com>
2022-02-22 18:43:15 +0100

    Hotfixes2.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: 'AIzaSyBIiIE4f3m5Bje3-AORaIbubE1D4dQTYZ0',
-    authDomain: 'vitis-riparia.firebaseapp.com',
-    projectId: 'vitis-riparia',
-    storageBucket: 'vitis-riparia.appspot.com',
-    messagingSenderId: '942582033052',
-    appId: '1:942582033052:web:54b9998b15480f6a765b59',
-    measurementId: 'G-KGZHEJD79M'
+
   },
   production: false
 };
